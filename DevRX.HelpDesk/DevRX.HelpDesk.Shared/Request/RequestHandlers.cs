6670bb9a29b4f8bae1ea7871571fe9fd995039c2
DirectumRXUser <directumrxuser@c.ru>
2024-04-29 04:45:55 +0500

    1. В табличной части отметок, при добавлении новой строки поле Сотрудник заполняется текущим сотрудником. 2. Поле с общей суммой часов закрыто для редактирования. Часы автоматически высчитываются на основании данных табличной части.  [Rx]

--- DevRX.HelpDesk/DevRX.HelpDesk.Shared/Request/RequestHandlers.cs
+++ DevRX.HelpDesk/DevRX.HelpDesk.Shared/Request/RequestHandlers.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using Sungero.Core;
 using Sungero.CoreEntities;
 using DevRX.HelpDesk.Request;
+using Sungero.Company;
 
 namespace DevRX.HelpDesk
 {
+  partial class RequestRequestActionsSharedCollectionHandlers
+  {
+
+    public virtual void RequestActionsAdded(Sungero.Domain.Shared.CollectionPropertyAddedEventArgs e)
+    {
+      _added.Employee = Employees.Current;
+    }
+  }
+
   partial class RequestSharedHandlers
   {
+
+    public virtual void RequestActionsChanged(Sungero.Domain.Shared.CollectionPropertyChangedEventArgs e)
+    {
+      _obj.HoursSum = _obj.RequestActions.Sum(x => x.HoursCount);
+    }
 
   }
 }
