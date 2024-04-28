200d25c98cdb511234ad61fafd5af33d5fa03681
DirectumRXUser <directumrxuser@c.ru>
2024-04-29 00:08:56 +0500

    Добавлено автозаполнение свойств "Сотрудник" и "Номер обращения". [Rx]

--- DevRX.HelpDesk/DevRX.HelpDesk.Server/Request/RequestHandlers.cs
+++ DevRX.HelpDesk/DevRX.HelpDesk.Server/Request/RequestHandlers.cs
@@ -4,7 +4,18 @@ using System.Linq;
 using Sungero.Core;
 using Sungero.CoreEntities;
 using DevRX.HelpDesk.Request;
+using Sungero.Company;
 
 namespace DevRX.HelpDesk
 {
+  partial class RequestServerHandlers
+  {
+
+    public override void Created(Sungero.Domain.CreatedEventArgs e)
+    {
+      _obj.Number = _obj.Id;
+      _obj.Responsible = Employees.Current;
+    }
+  }
+
 }
