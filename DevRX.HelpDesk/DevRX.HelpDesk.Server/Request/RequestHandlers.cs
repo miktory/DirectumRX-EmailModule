089d09a8bf303319bd4b60267869bf8da7f2e2b4
DirectumRXUser <directumrxuser@c.ru>
2024-04-29 04:58:59 +0500

    При создании обращения поле "Дата создания" заполняется текущей датой. В случае, если в поле "Жизненный цикл" находится значение "Закрыто" - дата закрытия также подставляется автоматически. [Rx]

--- DevRX.HelpDesk/DevRX.HelpDesk.Server/Request/RequestHandlers.cs
+++ DevRX.HelpDesk/DevRX.HelpDesk.Server/Request/RequestHandlers.cs
@@ -11,11 +11,18 @@ namespace DevRX.HelpDesk
   partial class RequestServerHandlers
   {
 
+    public override void BeforeSave(Sungero.Domain.BeforeSaveEventArgs e)
+    {
+      if (_obj.LifeCycle == LifeCycle.Closed)
+        _obj.ClosedDate = Calendar.Today;
+    }
+
     public override void Created(Sungero.Domain.CreatedEventArgs e)
     {
       _obj.Number = _obj.Id;
       _obj.Responsible = Employees.Current;
       _obj.LifeCycle = LifeCycle.InWork;
+      _obj.CreatedDate = Calendar.Today;
     }
   }
 
