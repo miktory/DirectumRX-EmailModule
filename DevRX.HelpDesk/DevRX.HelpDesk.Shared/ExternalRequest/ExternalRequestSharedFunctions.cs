d55a0e80144ed2900ab66ec8eb70051e34331fdb
DirectumRXUser <directumrxuser@c.ru>
2024-04-28 06:09:50 +0500

    Внесены изменения в абастрактный список. [Rx]

---
+++ DevRX.HelpDesk/DevRX.HelpDesk.Shared/ExternalRequest/ExternalRequestSharedFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DevRX.HelpDesk.ExternalRequest;
+
+namespace DevRX.HelpDesk.Shared
+{
+  partial class ExternalRequestFunctions
+  {
+
+  }
+}
