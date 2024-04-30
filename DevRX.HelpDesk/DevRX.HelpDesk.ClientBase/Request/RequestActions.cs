b686f2f102ff358cceb2e13b886c8a9a715c6261
DirectumRXUser <directumrxuser@c.ru>
2024-04-30 09:03:02 +0500

    Добавлена возможность переоткрыть закрытое ранее обращение [Rx]

--- DevRX.HelpDesk/DevRX.HelpDesk.ClientBase/Request/RequestActions.cs
+++ DevRX.HelpDesk/DevRX.HelpDesk.ClientBase/Request/RequestActions.cs
@@ -7,5 +7,22 @@ using DevRX.HelpDesk.Request;
 
 namespace DevRX.HelpDesk.Client
 {
+  partial class RequestActions
+  {
+    public virtual void OpenClosedRequest(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+        _obj.LifeCycle = LifeCycle.InWork;
+        foreach (var p in _obj.State.Properties)
+          p.IsEnabled = true;
+        _obj.ClosedDate = null;
+    }
+
+    public virtual bool CanOpenClosedRequest(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+       return _obj.LifeCycle == LifeCycle.Closed && !_obj.State.IsChanged;
+    }
+
+  }
+
 
 }
