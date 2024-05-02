f58a907ad4ed4684c36734002bdf1a34f8937f0b
DirectumRXUser <directumrxuser@c.ru>
2024-05-02 10:16:07 +0500

    Добавлен фоновый процесс, который каждую пятницу производит рассылку с информацией о зарегистрированных обращениях выбранным сотрудникам. [Rx]

--- DevRX.HelpDesk/DevRX.HelpDesk.Server/ModuleJobs.cs
+++ DevRX.HelpDesk/DevRX.HelpDesk.Server/ModuleJobs.cs
@@ -9,5 +9,20 @@ namespace DevRX.HelpDesk.Server
   public class ModuleJobs
   {
 
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual void SendNotificationAboutRequests()
+    {
+      var newRequests = Requests.GetAll(r => r.CreatedDate.Value > Calendar.Today.BeginningOfWeek());
+      // Получить исполнителя по задаче,
+      // в рамках данной практики - произвольный сотрудник.
+      var performer = Sungero.Company.Employees.GetAll().First();
+      // Создать и стартовать задачу.
+      var task= Sungero.Workflow.SimpleTasks.CreateWithNotices("Статистика по обращениям", performer);
+      task.ActiveText = "Зарегистрировано обращений: " + newRequests.Count();
+      task.Start();
+    }
+
   }
 }
