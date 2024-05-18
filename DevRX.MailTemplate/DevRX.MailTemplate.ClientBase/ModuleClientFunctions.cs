e3de273e2806a4e664d14075f6a46051c388d0dd
DirectumRXUser <directumrxuser@c.ru>
2024-05-18 20:50:09 +0500

    Добавлена обложка модуля [Rx]

--- DevRX.MailTemplate/DevRX.MailTemplate.ClientBase/ModuleClientFunctions.cs
+++ DevRX.MailTemplate/DevRX.MailTemplate.ClientBase/ModuleClientFunctions.cs
@@ -9,5 +9,15 @@ namespace DevRX.MailTemplate.Client
   public class ModuleFunctions
   {
 
+    /// <summary>
+    /// Отправить электронное письмо одному или нескольким получателям.
+    /// </summary>
+    /// 
+    [Public]
+    public virtual void SendMail()
+    {
+      MailTemplateSolution.Module.Docflow.PublicFunctions.Module.SendMail();
+    }
+
   }
 }
